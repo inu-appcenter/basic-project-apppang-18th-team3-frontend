ea49aedc838f5dc7e3e8d563cbdc37b5c583c5d6
Congee <guswogus81@gmail.com>
2026-07-08 11:49:56 +0900

    Fix build.sh: exclude .git with rsync

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./basic-project-apppang-18th-team3-frontend/* ./output
-cp -R ./basic-project-apppang-18th-team3-frontend/.[!.]* ./output
+rsync -a --exclude='.git' --exclude='output' ./basic-project-apppang-18th-team3-frontend/ ./output/
 cp -R ./output ./basic-project-apppang-18th-team3-frontend/
